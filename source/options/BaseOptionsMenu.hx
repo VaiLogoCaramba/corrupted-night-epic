08f75be6932e63eeb57e274c5c19228f4f941dd5
VaiLogoCaramba <150534279+VaiLogoCaramba@users.noreply.github.com>
2024-03-17 02:17:20 -0300

    Update BaseOptionsMenu.hx

--- source/options/BaseOptionsMenu.hx
+++ source/options/BaseOptionsMenu.hx
@@ -132,6 +132,10 @@ class BaseOptionsMenu extends MusicBeatSubstate
 
 		changeSelection();
 		reloadCheckboxes();
+
+		#if android
+		addVirtualPad(LEFT_FULL, A_B_C);
+		#end
 	}
 
 	public function addOption(option:Option) {
